fb5ff3c0baf58625b04ff7c83c74415d4e093346
Massimiliano Arione <garakkio@gmail.com>
2018-05-13 17:37:07 +0200

    improve Configuration

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -24,8 +24,6 @@ class Configuration implements ConfigurationInterface
                 ->scalarNode('tag_class')
                     ->isRequired()
                 ->end()
-            ->end()
-            ->children()
                 ->booleanNode('purge')
                     ->defaultFalse()
                 ->end()

2067349299df0c87c1ebc95edeb76bc9f49f069d
Massimiliano Arione <garakkio@gmail.com>
2019-03-20 19:05:22 +0100

    fix deprecation

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -5,17 +5,13 @@ namespace Beelab\TagBundle\DependencyInjection;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
-/**
- * This is the class that validates and merges configuration from configuration files.
- *
- * To learn more see {@link http://symfony.com/doc/current/bundles/extension.html}
- */
-class Configuration implements ConfigurationInterface
+final class Configuration implements ConfigurationInterface
 {
     public function getConfigTreeBuilder(): TreeBuilder
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('beelab_tag');
+        $treeBuilder = new TreeBuilder('beelab_tag');
+        // BC layer for symfony/config < 4.2
+        $rootNode = \method_exists($treeBuilder, 'getRootNode') ? $treeBuilder->getRootNode() : $treeBuilder->root('beelab_tag');
         $rootNode
             ->children()
                 ->scalarNode('tag_class')
